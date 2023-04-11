c50ef6460414cf81c19dceb5dd033c65d7f7128e
Qwaieare <gb-grand@mail.ru>
2023-04-11 20:15:45 +0300

    courseWorkOfTheThird_20230411 - повтор

---
+++ src/main/java/skypro/internetshopsocks/CourseWorkOfTheThird20230411Application.java
@@ -0,0 +1,13 @@
+package skypro.internetshopsocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkOfTheThird20230411Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkOfTheThird20230411Application.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/internetshopsocks/CourseWorkOfTheThird20230411ApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.internetshopsocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkOfTheThird20230411ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
